68aa0702e99a6bb32ba26004a86e572c4f74dc31
Ayen Bonsoa <bonsoa.yen@gmail.com>
2023-04-25 13:55:51 +0000

    feat:add Navbar and Hero component

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,15 @@
+"use client";
+
 import Image from "next/image";
 import Link from "next/link";
+import Navbar from "@/components/ui/sections/Navbar";
+import Hero from "@/components/ui/sections/Hero";
 
 export default function Home() {
   return (
-    <div>
-      <Link href="/dashboard">Dashboard</Link>
+    <div className="font-mono">
+      <Navbar />
+      <Hero />
     </div>
   );
 }
